a47f624f653ced57d9f9ea4ce6d0fd2fdebe6e8f
abirhossain <amee11315@gmail.com>
2018-05-28 22:15:56 +0600

    added minimum sdk in the manifest

--- app/src/main/java/com/abiramee/gradientview/MainActivity.java
+++ app/src/main/java/com/abiramee/gradientview/MainActivity.java
@@ -8,8 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.abiramee1.gradientview.GradientView;
-
 public class MainActivity extends AppCompatActivity {
 
     private GradientView mGradientView;
